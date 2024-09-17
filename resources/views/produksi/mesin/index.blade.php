04923ad8c41c16fa9f511235cf86c627cc254bc8
huda32 <mohnurulhuda32@gmail.com>
2024-09-17 15:57:28 +0630

    update template

--- resources/views/produksi/mesin/index.blade.php
+++ resources/views/produksi/mesin/index.blade.php
@@ -1,62 +1,103 @@
 @extends('layouts.master')
-
+@push('styles')
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endpush
 @section('content')
 
-
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      {{-- <h1 class="h3 mb-0 text-gray-800">Dashboard</h1> --}}
-      <a href="/mesin/create" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fa fa-plus fa-sm text-white-50"></i> ADD</a>
-      <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-    </div>
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-    </div>
-    <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Mesin</th>
-                        <th>Merk</th>
-                        <th>Type</th>
-                        <th>No Mesin</th>
-                        <th>SN Mesin</th>
-                        <th>Lokasi</th>
-                        <th>KW</th>
-                        <th>Tahun</th>
-                        <th>Jenis</th>
-                        <th>Kategori Mesin</th>
-                        <th>Kondisi Mesin</th>
-                        <th>Teknisii</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">DataTable with default features</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="example1" class="table table-bordered table-striped ">
+            <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Mesin</th>
+                  <th>Merk</th>
+                  <th>Type</th>
+                  <th>No Mesin</th>
+                  <th>SN Mesin</th>
+                  <th>Lokasi</th>
+                  <th>KW</th>
+                  <th>Tahun</th>
+                  <th>Jenis</th>
+                  <th>Kategori Mesin</th>
+                  <th>Kondisi Mesin</th>
+                  <th>Teknisii</th>
+              </tr>
+            </thead>
+            <tbody>
                     
-                    @foreach ($mesins as $mesin)
-             
-                        <tr>
-                            <td>{{ $nomer++}}</td>
-                            <td><a href="/mesin/{{$mesin->id}}">{{ $mesin->nama_mesin}}</a></td>
-                            <td>{{ $mesin->merk_mesin->merk_mesin}}</td>
-                            <td>{{ $mesin->type_mesin}}</td>
-                            <td>{{ $mesin->no_mesin}}</td>
-                            <td>{{ $mesin->sn_mesin}}</td>
-                            <td>{{ $mesin->gedung->nama_gedung}}</td>
-                            <td>{{ $mesin->kw}}</td>
-                            <td>{{ $mesin->tahun}}</td>
-                            <td>{{ $mesin->jenis_mesin->jenis_mesin}}</td>
-                            <td>{{ $mesin->kategori_mesin->kategori_mesin}}</td>
-                            <td>{{ $mesin->status->status}}</td>
-                            <td>{{ $mesin->teknisi_mesin->nama_teknisi}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-                </table>
-              </div>
-             
+              @foreach ($mesins as $mesin)
+       
+                  <tr>
+                      <td>{{ $nomer++}}</td>
+                      <td><a href="/mesin/{{$mesin->id}}">{{ $mesin->nama_mesin}}</a></td>
+                      <td>{{ $mesin->merk_mesin->merk_mesin}}</td>
+                      <td>{{ $mesin->type_mesin}}</td>
+                      <td>{{ $mesin->no_mesin}}</td>
+                      <td>{{ $mesin->sn_mesin}}</td>
+                      <td>{{ $mesin->gedung->nama_gedung}}</td>
+                      <td>{{ $mesin->kw}}</td>
+                      <td>{{ $mesin->tahun}}</td>
+                      <td>{{ $mesin->jenis_mesin->jenis_mesin}}</td>
+                      <td>{{ $mesin->kategori_mesin->kategori_mesin}}</td>
+                      <td>{{ $mesin->status->status}}</td>
+                      <td>{{ $mesin->teknisi_mesin->nama_teknisi}}</td>
+                  </tr>
+              @endforeach
+          </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
     </div>
+    <!-- /.col -->
   </div>
+  <!-- /.row -->
+</div>
 @endsection
+
+@push('scripts')
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{ asset('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{ asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{ asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+
+@endpush
